54fb1a8fe9677a9d76e0a997242da14756ff6760
shubhdavps <shubhamdavpsbseb@gmail.com>
2025-10-05 19:40:03 +0530

    Update app.py

--- test/app.py
+++ test/app.py
@@ -32,15 +32,3 @@ def logout():
 if __name__ == "__main__":
     app.run(debug=True)
 
-import os
-from flask import Flask
-app = Flask(__name__)
-
-PORT = int(os.environ.get("PORT", 5000))
-
-@app.route("/")
-def home():
-    return "Hello World!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
